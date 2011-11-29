0a03baf266ee7850430f1ef5371579ac7fb3e702
Wasif Aleem <wasifaleem30@gmail.com>
2011-11-29 18:23:12 +0530

    initial commit

---
+++ Mac Changer/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Mac_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(117, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(288, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Card";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(117, 75);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(156, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Mac";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Current MAC: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(500, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Select Card First";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(466, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Remove from list";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(117, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Change";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(589, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(466, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(279, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(589, 76);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 177);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "MAC Changer";
+            this.Load += new System.EventHandler(this.Form1Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Mac Changer/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+
+namespace Mac_Changer
+{
+    public partial class Form1 : Form
+    {
+        private XmlDocument _doc;
+        private const string Path = @"MAC.xml";
+        public Form1()
+        {
+            InitializeComponent();
+            GetInterfaces();
+            GetMac();
+        }
+        public class Language
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public override string ToString() { return Name; }
+        }
+        public void GetMac()
+        {
+            XDocument xmlDoc = XDocument.Load(Path);
+            var q = from c in xmlDoc.Descendants("Mac") let xElement = c.Element("Value") where xElement != null let element = c.Element("Id") where element != null select new { Value = xElement.Value, Id = element.Value };
+            var dict = q.ToDictionary(obj => obj.Value, obj => Convert.ToInt32(obj.Id));
+            comboBox2.DataSource = new BindingSource(dict, null);
+            comboBox2.DisplayMember = "Key";
+            comboBox2.ValueMember = "Value";
+            comboBox2.Focus();
+        }
+        public void GetInterfaces()
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet || nic.NetworkInterfaceType == NetworkInterfaceType.FastEthernetFx || nic.NetworkInterfaceType == NetworkInterfaceType.FastEthernetT
+)
+                {
+                    PhysicalAddress physicalAddress = nic.GetPhysicalAddress();
+                    dict.Add(nic.Description, physicalAddress.ToString());
+                }
+            }
+            comboBox1.DataSource = new BindingSource(dict, null);
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
+            comboBox1.Focus();
+            label4.Text = comboBox1.SelectedValue.ToString();
+        }
+        public void SetMac(string macAddress)
+        {
+            const string Name = @"SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002bE10318}";
+            using (RegistryKey key0 = Registry.LocalMachine.OpenSubKey(Name, RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl))
+            {
+
+                string[] x = key0.GetSubKeyNames();
+                foreach (string name in x)
+                {
+
+                        var var1 = Registry.LocalMachine.OpenSubKey(Name,RegistryKeyPermissionCheck.ReadWriteSubTree,RegistryRights.FullControl);
+                        var v = var1.OpenSubKey(name, RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl);
+                        var z = v.GetValue("DriverDesc");
+                        if (comboBox1.Text == z.ToString() )
+                        {
+                            v.SetValue("NetworkAddress",comboBox2.Text);
+                            MessageBox.Show(z.ToString());
+                        }
+                        v.Close();
+                        var1.Close();
+
+                }
+                key0.Close();
+            }
+
+        }
+        public void Reset()
+        {
+            ManagementObjectSearcher mos = null;
+            mos = new ManagementObjectSearcher(@"SELECT * 
+                                     FROM   Win32_NetworkAdapter 
+                                     WHERE  Description = '"+comboBox1.Text+"'");
+            ManagementObjectCollection v = mos.Get();
+
+            foreach (ManagementObject mo in v)
+            {
+                string[] args= {};
+                mo.InvokeMethod("Disable",args);
+                mo.InvokeMethod("Enable", args);
+            }
+
+
+        }
+
+        private void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.Text = comboBox1.SelectedValue.ToString();
+        }
+
+        public void Form1Load(object sender, EventArgs e)
+        {
+
+            //Create an xml document
+            _doc = new XmlDocument();
+
+            //If there is no current file, then create a new one
+            if (!File.Exists(Path))
+            {
+                //Create neccessary nodes
+                XmlDeclaration declaration = _doc.CreateXmlDeclaration("1.0", "UTF-8", "yes");
+                XmlComment comment = _doc.CreateComment("This is an XML Generated File");
+                XmlElement root = _doc.CreateElement("Root");
+                _doc.AppendChild(declaration);
+                _doc.AppendChild(comment);
+                _doc.AppendChild(root);
+                _doc.Save(Path);
+            }
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+
+            const string pattern = @"^([0-9a-fA-F][0-9a-fA-F]){5}([0-9a-fA-F][0-9a-fA-F])$";
+
+            if (Regex.IsMatch((textBox1.Text), pattern))
+            {
+                //Load the XML File
+                _doc.Load(Path);
+
+                //Get the root element
+                XmlElement root = _doc.DocumentElement;
+
+                //XmlElement person = doc.CreateElement("Person");
+                XmlElement mac = _doc.CreateElement("Mac");
+                XmlElement value = _doc.CreateElement("Value");
+                XmlElement id = _doc.CreateElement("Id");
+
+                //Add the values for each nodes
+                value.InnerText = PhysicalAddress.Parse(textBox1.Text.ToUpper()).ToString();
+                var rnd = new Random();
+                id.InnerText = rnd.Next(0, int.MaxValue).ToString();
+
+                //Construct the value element
+                mac.AppendChild(value);
+                mac.AppendChild(id);
+
+                //Add the New mac element to the end of the root element
+                root.AppendChild(mac);
+
+                //Save the document
+                _doc.Save(Path);
+                MessageBox.Show("Added Succesfully", "Mac Changer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input Format\nEnter Address without spaces\nExample:  ff00ff00ff00", "MAC Changer", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            GetMac();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetMac(comboBox2.SelectedValue.ToString());
+            Reset();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+    }
+}
